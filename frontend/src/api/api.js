df2717d25cae354f7937289f61b09cbd849ebdbc
Geyamrutha Poluru <144701596+geyamrutha16@users.noreply.github.com>
2026-02-15 20:24:17 +0530

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 // api/api.js
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:3000"; // Your backend URL
+const API_BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
 export const api = axios.create({
     baseURL: API_BASE_URL,
